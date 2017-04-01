f9ade9ed60c34692379b967313907d3146e93a41
alexsolovyova <solovyovaa@yahoo.com>
2017-04-01 10:29:09 +0300

    eq

---
+++ src/by/htp/equipment/Equipment.java
@@ -0,0 +1,26 @@
+package by.htp.equipment;
+
+
+public class Equipment {
+	
+	private String bicycle;
+	private String rollers;
+	private String skateboard;
+	private String helmet;
+	private String kneepads;
+	private int size;
+	private int weight; 
+
+	public Equipment (String bicycle, String rollers, String skateboard, String helmet, String kneepads, int size, int weight ) {
+		
+		this.bicycle = bicycle;
+		this.rollers = rollers;
+		this.skateboard = skateboard;
+		this.helmet = helmet;
+		this.kneepads = kneepads;
+		this.size = size;
+		this.weight = weight;
+	}
+	
+	
+}
---
+++ src/by/htp/equipment/Main.java
@@ -0,0 +1,10 @@
+package by.htp.equipment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
