b986448a5eee1459857b2ec1a5fdac3102f41210
alexsolovyova <solovyovaa@yahoo.com>
2017-04-06 18:31:39 +0300

    Add files via upload

--- src/by/htp/equipment/Bicycle.java
+++ src/by/htp/equipment/Bicycle.java
@@ -6,8 +6,8 @@ public class Bicycle extends Equipment {
 private String type;
 	
 	public Bicycle (String title, double price,
-			double weight, String color, String type) {
-		super(title, price, weight,color);
+			 String color, String type) {
+		super();
 		this.type = type;
 	
 	}
